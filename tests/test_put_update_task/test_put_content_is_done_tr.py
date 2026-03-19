996e9e49494e24d3add8e18aff5acefd5a721a04
Alekseno <aleksenocode@gmail.com>
2026-03-19 22:26:40 +0300

    update_is_done_true

--- tests/test_put_update_task/test_put_content_is_done_tr.py
+++ tests/test_put_update_task/test_put_content_is_done_tr.py
@@ -1,31 +1,32 @@
 import httpx
-from secondary_func import create_task, update_task, get_task_id
+from secondary_func import create_task, get_task_id, update_task
 
-def test_put_update_task(): #создаем задачу
+def test_put_content_is_done_tr():
     response = create_task(
-        content = "список покупок на субботу",
+        content = "5055",
         user_id = str,
         is_done = False
     )
 
-    create_data = response.json()#вытаскиваем данные
-     
+    create_data = response.json()
+
     create_task_id = create_data['task']['task_id']
     create_user_id = create_data['task']['user_id']
 
     put_response = update_task(
-        content = "покупки на вторник",#обновляем задачу
+        content = "100",
         user_id = create_user_id,
         task_id = create_task_id,
-        is_done = False
+        is_done = True
     )
-    update_data = put_response.json()#получаем данные
-    put_task_id = update_data['updated_task_id']#получаем task_id
-    
+
+    update_data = put_response.json()
+    put_task_id = update_data['updated_task_id']
+
     assert put_response.status_code == httpx.codes.OK, f"Ожидался код 200, пришел {put_response.status_code}"
 
     get_task_response = get_task_id(
-        task_id = put_task_id#отправялем task_id, чтоб получить body
+        task_id = put_task_id
     )
 
     get_task_id_data = get_task_response.json()
@@ -34,3 +35,4 @@ def test_put_update_task(): #создаем задачу
 
     assert get_task_id_data['user_id'] == create_user_id, "is different"
     assert get_task_id_data['task_id'] == create_task_id, "is different"
+    assert get_task_id_data['is_done'] == True, "is different"
