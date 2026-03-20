73add01337e029e5ab25a69c74cbade9f854da22
Alekseno <aleksenocode@gmail.com>
2026-03-20 17:30:26 +0300

    task str

---
+++ tests/test_create_task/test_create_content_task_None.py
@@ -0,0 +1,14 @@
+import httpx
+from secondary_func import create_task
+
+def test_create_content_task_None():
+    response = create_task(
+        content = "task_None",
+        user_id = str,
+        is_done = False
+    )
+
+    data_response = response.json()
+    create_task_id = data_response['task']['task_id']
+
+    assert create_task_id == str(create_task_id), "is not str"
