e9e47367f115c4ec0b23b994c54ded3b2fcf3f47
Alekseno <aleksenocode@gmail.com>
2026-03-20 16:00:20 +0300

    del x2, N

---
+++ tests/test_DEL_task_id/test_del_task_N.py
@@ -0,0 +1,24 @@
+import httpx
+from secondary_func import create_task, del_task
+
+def test_del_task_N():
+    response = create_task(
+        content = "del negativ task",
+        user_id = str,
+        is_done = False
+    )
+
+    create_data = response.json()
+    create_task_id = create_data['task']['task_id'][:8]
+
+    del_response = del_task(create_task_id)
+    del_data = del_response.json()
+
+    assert del_response.status_code == httpx.codes.OK, f"Ожидался код 200, пришел {del_response.status_code}"
+
+    assert del_data['deleted_task_id'] == create_task_id
+
+
+
+
+
---
+++ tests/test_DEL_task_id/test_del_task_x2.py
@@ -0,0 +1,27 @@
+import httpx
+from secondary_func import create_task, get_task_id, del_task
+
+def test_del_task_x2():
+    response = create_task(
+        content = "delete task x2",
+        user_id = str,
+        is_done = False
+    )
+
+    create_data = response.json()
+    create_task_id = create_data['task']['task_id']
+
+    del_response = del_task(create_task_id)
+    del_data = del_response.json()
+
+    assert del_response.status_code == httpx.codes.OK, f" Ожидался код 200, пришел {del_response.status_code}"
+
+    assert del_data['deleted_task_id'] == create_task_id
+
+    del_x2_response = del_task(create_task_id)
+    del_x2_data = del_x2_response.json()
+
+    assert del_x2_response.status_code == httpx.codes.OK, f"Ожидался код 200, пришел {del_x2_response.status_code}"
+
+    assert del_x2_data['deleted_task_id'] == create_task_id
+
