b0e45648e9a1e5466cbf39802cfef8ae2cb748bf
Alekseno <aleksenocode@gmail.com>
2026-04-12 18:38:11 +0300

    expected_result 500

--- tests/test_create_task/test_create_minus_user_id.py
+++ tests/test_create_task/test_create_minus_user_id.py
@@ -7,9 +7,9 @@ def test_minus_user_id():
         is_done = False
     )
     expected_result = 'Internal Server Error'
-    assert minus_user_ids.status_code == httpx.codes.INTERNAL_SERVER_ERROR, f'Ожидался код 500, пришел {minus_user_ids.status_code}'
+    assert minus_user_ids.status_code == httpx.codes.INTERNAL_SERVER_ERROR, f"Ожидался код 500, пришел {minus_user_ids.status_code}"
 
-    assert minus_user_ids.text == expected_result, f"Изменился код ошибки(был 500)"
+    assert minus_user_ids.text == expected_result, f"Ожидался текст ошибки {expected_result}, пришел {minus_user_ids.text}"
 
 
     
